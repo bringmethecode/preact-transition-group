ecf3a9cb9992401075d58a1162f773cd2a91407f
Andre Wiggins <andrewiggins@live.com>
2018-09-23 23:06:34 -0700

    Convert ReplaceTransition to preact and rewrite buble unsupported language features

--- src/ReplaceTransition.js
+++ src/ReplaceTransition.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { findDOMNode } from 'react-dom';
+import { Component, h, cloneElement } from 'preact';
 import TransitionGroup from './TransitionGroup';
 
 /**
@@ -13,27 +12,32 @@ import TransitionGroup from './TransitionGroup';
  * </ReplaceTransition>
  * ```
  */
-class ReplaceTransition extends React.Component {
-	handleEnter = (...args) => this.handleLifecycle('onEnter', 0, args);
-	handleEntering = (...args) => this.handleLifecycle('onEntering', 0, args);
-	handleEntered = (...args) => this.handleLifecycle('onEntered', 0, args);
+class ReplaceTransition extends Component {
+	constructor(props, context) {
+		super(props, context);
 
-	handleExit = (...args) => this.handleLifecycle('onExit', 1, args);
-	handleExiting = (...args) => this.handleLifecycle('onExiting', 1, args);
-	handleExited = (...args) => this.handleLifecycle('onExited', 1, args);
+		this.handleEnter = (...args) => this.handleLifecycle('onEnter', 0, args);
+		this.handleEntering = (...args) => this.handleLifecycle('onEntering', 0, args);
+		this.handleEntered = (...args) => this.handleLifecycle('onEntered', 0, args);
+
+		this.handleExit = (...args) => this.handleLifecycle('onExit', 1, args);
+		this.handleExiting = (...args) => this.handleLifecycle('onExiting', 1, args);
+		this.handleExited = (...args) => this.handleLifecycle('onExited', 1, args);
+	}
 
 	handleLifecycle(handler, idx, originalArgs) {
 		const { children } = this.props;
-		const child = React.Children.toArray(children)[idx];
+		const child = children[idx];
 
 		if (child.props[handler]) child.props[handler](...originalArgs);
-		if (this.props[handler]) this.props[handler](findDOMNode(this));
+		if (this.props[handler]) this.props[handler](this.base);
 	}
 
 	render() {
 		const { children, in: inProp, ...props } = this.props;
-		const [first, second] = React.Children.toArray(children);
+		const [first, second] = children;
 
+		// TODO: Use destructuring to remove these props
 		delete props.onEnter;
 		delete props.onEntering;
 		delete props.onEntered;
@@ -44,13 +48,13 @@ class ReplaceTransition extends React.Component {
 		return (
 			<TransitionGroup {...props}>
 				{inProp
-					? React.cloneElement(first, {
+					? cloneElement(first, {
 						key: 'first',
 						onEnter: this.handleEnter,
 						onEntering: this.handleEntering,
 						onEntered: this.handleEntered
 					  })
-					: React.cloneElement(second, {
+					: cloneElement(second, {
 						key: 'second',
 						onEnter: this.handleExit,
 						onEntering: this.handleExiting,
